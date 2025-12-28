78945a24586406771c16ec042018f74c9a237ec2
Turkmen777 <artyomgurbangulyyew1998@gmail.com>
2025-12-28 20:53:01 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Ertir - Blogy</h1>
+      <p>Hoş geldiňiz! Türkmenistan üçin blog.</p>
+      <ul>
+        <li>Täze habarlar</li>
+        <li>Sport</li>
+        <li>Tehnologiýalar</li>
+        <li>Medeniýet</li>
+        <li>Saglyk</li>
+        <li>Bilim</li>
+        <li>Ýaşlar</li>
+      </ul>
+    </div>
+  )
+}
